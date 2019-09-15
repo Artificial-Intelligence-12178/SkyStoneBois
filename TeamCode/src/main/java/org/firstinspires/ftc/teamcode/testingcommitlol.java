1b19a0360bbc4afca090e39124514484162ef34b
Jaysb <jriveraschool01@gmail.com>
2019-09-15 10:26:07 -0400

    lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingcommitlol.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingcommitlol.java
@@ -9,4 +9,8 @@ public class testingcommitlol {
     public static void tryThisMethod(){
         System.out.println("i really hope this doesnt break my android studio");
     }
+
+    public static int returnARandomInt(){
+        return (int)(Math.random()*Integer.MAX_VALUE);
+    }
 }
