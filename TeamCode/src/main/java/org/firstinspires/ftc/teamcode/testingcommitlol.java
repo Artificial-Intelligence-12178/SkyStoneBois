8f878a1c137c41a8bbdcf213fe2fe3ad0a7c390f
Jaysb <jriveraschool01@gmail.com>
2019-09-14 23:35:13 -0400

    lol

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingcommitlol.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class testingcommitlol {
+    public static void main(String[] args) {
+        System.out.println("if kim reads this she has to make justin head programmer");
+        tryThisMethod();
+    }
+
+    public static void tryThisMethod(){
+        System.out.println("i really hope this doesnt break my android studio");
+    }
+}
