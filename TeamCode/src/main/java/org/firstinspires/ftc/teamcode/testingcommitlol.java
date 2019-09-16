105a8e12acab356a188f32f3680b996ef449634c
Kimberly <kimberlytoledo36@gmail.com>
2019-09-15 22:29:17 -0400

    test test commit 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingcommitlol.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingcommitlol.java
@@ -1,16 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
-public class testingcommitlol {
-    public static void main(String[] args) {
-        System.out.println("if kim reads this she has to make justin head programmer");
+public class testingcommitlol
+{
+    public static void main(String[] args)
+    {
+        System.out.println("if justin reads this he has small pp energy");
         tryThisMethod();
+        isJustinBigBrain();
     }
 
-    public static void tryThisMethod(){
+    public static void tryThisMethod()
+    {
         System.out.println("i really hope this doesnt break my android studio");
     }
 
-    public static int returnARandomInt(){
+    public static int returnARandomInt()
+    {
         return (int)(Math.random()*Integer.MAX_VALUE);
     }
+
+    public static boolean isJustinBigBrain()
+    {
+        return true;
+    }
 }
+
+//I FIXED YO BRACKETS
