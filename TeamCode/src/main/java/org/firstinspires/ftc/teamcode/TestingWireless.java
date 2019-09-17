577ef4903bb6c0fa521056c2a76eea797316e82f
Kimberly <kimberlytoledo36@gmail.com>
2019-09-17 15:37:22 -0400

    testubg wurekess

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingWireless.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "Testing 123")
+
+public class TestingWireless extends LinearOpMode
+{
+    @Override
+    public void runOpMode()
+    {
+        telemetry.addLine("hi do you see me");
+        waitForStart();
+
+        telemetry.addLine("she here now");
+    }
+}
