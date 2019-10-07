e9a1ee6f25165bda186780ac0f20c6fbf615c2b9
abeashvil <abrahama9796@halsey157.com>
2019-10-07 16:06:23 -0400

    poop fart butt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/DriverClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/DriverClass.java
@@ -4,9 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Helpers.Joe;
+
+import java.sql.SQLOutput;
 
 @TeleOp(name = "Official Drive")
 
@@ -37,13 +37,13 @@ public class DriverClass extends OpMode
     @Override
     public void loop()
     {
-        //getting the angle (in radians)
+        //Getting the angle (in radians)
         double x = gamepad1.left_stick_x;
         double y = gamepad1.left_stick_y;
         double angleRad = Math.abs(Math.atan(y / x));
         double degree;
 
-        //finding the reference angle
+        //Finding the reference angle
         if(x < 0 && y > 0)  //quad 2
             angleRad= Math.PI - angleRad;
         else if(x < 0 && y <= 0)  //quad 3
@@ -51,7 +51,7 @@ public class DriverClass extends OpMode
         else if(x >= 0 && y < 0)  //quad 4
             angleRad = 2*Math.PI - angleRad;
 
-        //determining power
+        //Determining power
         double vec = Math.sqrt(x*x+y*y);
         if(vec > 1)
             vec = 1;
@@ -59,7 +59,7 @@ public class DriverClass extends OpMode
         pow1 = vec*Math.sin(angleRad+Math.PI/4);
         pow2 = vec*Math.sin(angleRad-Math.PI/4);
 
-        //displaying information
+        //Displaying information
         degree = angleRad*(180/Math.PI);
         telemetry.addData("Degree:" , degree);
         telemetry.addData("pow1 = ", pow1);
@@ -67,10 +67,86 @@ public class DriverClass extends OpMode
         telemetry.update();
 
 
+        double rTrig = gamepad1.right_trigger;
+        double lTrig = gamepad1.left_trigger;
+        double xJoystick = gamepad1.left_stick_x;
+        double yJoystick = gamepad1.left_stick_y;
 
 
+        //Rotate right
+        if(rTrig != 0)
+        {
+            joe.frontL.setPower(rTrig);
+            joe.backL.setPower(rTrig);
+            joe.frontR.setPower(-rTrig);
+            joe.backR.setPower(-rTrig);
+        }
+        //Rotate left
+        else if(lTrig != 0)
+        {
+            joe.frontL.setPower(-rTrig);
+            joe.backL.setPower(-rTrig);
+            joe.frontR.setPower(rTrig);
+            joe.backR.setPower(rTrig);
+        }
+
+        //movement forward
+        if((xJoystick <=  .05 && xJoystick >= -.05) && yJoystick<0)
+        {
+            joe.frontL.setPower(vec);
+            joe.backL.setPower(vec);
+            joe.frontR.setPower(vec);
+            joe.backR.setPower(vec);
+        }
+        //movement backwards
+        else if((xJoystick <=  .05 && xJoystick >= -.05) && yJoystick>0)
+        {
+            joe.frontL.setPower(vec);
+            joe.backL.setPower(vec);
+            joe.frontR.setPower(vec);
+            joe.backR.setPower(vec);
+        }
+        //movement right
+        else if((yJoystick <=  .05 && yJoystick >= -.05) && xJoystick>0)
+        {
+            joe.frontL.setPower(vec);
+            joe.backL.setPower(-vec);
+            joe.frontR.setPower(-vec);
+            joe.backR.setPower(vec);
+        }
+        //movement left
+        else if((yJoystick <=  .05 && yJoystick >= -.05) && xJoystick<0)
+        {
+            joe.frontL.setPower(-vec);
+            joe.backL.setPower(vec);
+            joe.frontR.setPower(vec);
+            joe.backR.setPower(-vec);
+        }
+        //adjustable values (for leeway) H
+        //diagonal north east  .707, -.707
+        else if((yJoystick >= -.757 && yJoystick <= -.657) && (xJoystick <=  .757 && xJoystick >= .657))
+        {
+            joe.frontL.setPower(vec);
+            joe.backR.setPower(vec);
+        }
+        //diagonal north west -.707 , -.707
+        else if((yJoystick >= -.757 && yJoystick <= -.657) && (xJoystick >=  -.757 && xJoystick <= -.657))
+        {
+            joe.frontR.setPower(vec);
+            joe.backL.setPower(vec);
+        }
+        //diagonal south east
+        else if((yJoystick <= .757 && yJoystick >= .657) && (xJoystick <=  .757 && xJoystick >= .657))
+        {
+            joe.frontR.setPower(-vec);
+            joe.backL.setPower(-vec);
+        }
+        else if((yJoystick <= .757 && yJoystick >= .657) && (xJoystick >=  -.757 && xJoystick <= -.657))
+        {
+            joe.frontL.setPower(-vec);
+            joe.backR.setPower(-vec);
+        }
     }
-
 }
 
 
