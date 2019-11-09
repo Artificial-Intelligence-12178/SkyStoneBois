75d0d5f1bda7b44dff748671db2ef6c8168268ea
erie <jriveraschool01@gmail.com>
2019-11-09 17:34:07 -0500

    cleenn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RandomTests/TensorFlowDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RandomTests/TensorFlowDetection.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.Code;
+package org.firstinspires.ftc.teamcode.RandomTests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
