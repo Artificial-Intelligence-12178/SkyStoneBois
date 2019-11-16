17c923b0d6129f5dd892464e00e00828ccf67793
abeashvil <abrahama9796@halsey157.com>
2019-11-16 09:40:15 -0500

    Merge branch 'master' of C:\Users\thesl\StudioProjects\SkyStoneBois with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helpers/AutoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helpers/AutoBot.java
@@ -6,9 +6,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.Code.AutoStonePark;
-import org.firstinspires.ftc.teamcode.Code.IterativeAutoStonePark;
-
 public class AutoBot
 {
     //declaring DcMotors
@@ -93,8 +90,6 @@ public class AutoBot
         else
         {
             //advance step or stop
-            stop();
-            IterativeAutoStonePark.steps++;
         }
 
     }
@@ -111,8 +106,6 @@ public class AutoBot
         else
         {
             //advance step or stop
-            stop();
-            IterativeAutoStonePark.steps++;
         }
     }
 
@@ -127,8 +120,6 @@ public class AutoBot
         else
         {
             //advance step or stop
-            stop();
-            IterativeAutoStonePark.steps++;
         }
     }
 
@@ -143,8 +134,6 @@ public class AutoBot
         else
         {
             //advance step or stop
-            stop();
-            IterativeAutoStonePark.steps++;
         }
     }
 
@@ -158,8 +147,6 @@ public class AutoBot
         else
         {
             //advance step or stop
-            stop();
-            IterativeAutoStonePark.steps++;
         }
     }
 
@@ -173,42 +160,9 @@ public class AutoBot
         else
         {
             //advance step or stop
-            stop();
-            IterativeAutoStonePark.steps++;
         }
     }
 
-    public void stop(){
-        frontR.setPower(0);
-        frontL.setPower(0);
-        backR.setPower(0);
-        backL.setPower(0);
-        resetEncoder();
-    }
 
-    public int inchesToTicks(int in){
-        return (int)((960*in)/(3.93701*Math.PI));
-        /*return (int)((1440 * (2/3) * in) /
-                (3.93701 * 3.1415));*/
-    }
 
-    public void testForward(int inches, double pow){
-        int ticks = inchesToTicks(inches);
-        if(Math.abs(frontR.getCurrentPosition()) < ticks)
-        {
-
-            if(Math.abs(frontR.getCurrentPosition()) > ticks*.6)
-                pow/=2;
-            frontR.setPower(-pow);
-            frontL.setPower(pow);
-            backR.setPower(-pow);
-            backL.setPower(pow);
-        }
-        else
-        {
-            //advance step or stop
-            stop();
-            IterativeAutoStonePark.steps++;
-        }
-    }
 }
