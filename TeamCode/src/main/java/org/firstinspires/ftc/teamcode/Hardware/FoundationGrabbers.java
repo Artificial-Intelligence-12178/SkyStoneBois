5d9c6f871e9e0300c39ea646a7f76fb5cfdf3677
jriv01 <jriveraschool01@gmail.com>
2020-01-22 05:06:06 -0500

    Hardware Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/FoundationGrabbers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/FoundationGrabbers.java
@@ -5,18 +5,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class FoundationGrabbers {
 
+    //Servos for each grabber
     Servo leftGrabber;
     Servo rightGrabber;
 
+    //Global positions
     final double LEFT_DOWN = 0.5;
     final double LEFT_UP = 0.5;
 
     final double RIGHT_DOWN = 0.5;
     final double RIGHT_UP = 0.5;
 
+    //String to display status of each component
     protected String status = "";
 
     public FoundationGrabbers(HardwareMap map){
+        //Initializing components
         try {
             leftGrabber = map.get(Servo.class, "ServoL");
         } catch (Exception e) {
@@ -30,13 +34,16 @@ public class FoundationGrabbers {
         }
     }
 
+    //Method used to get the status of this FoundationGrabbers
     public String getStatus(){ return status; }
 
+    //Method used to move grabbers down
     public void grabbersDown(){
         leftGrabber.setPosition(LEFT_DOWN);
         rightGrabber.setPosition(RIGHT_DOWN);
     }
 
+    //Method used to move grabbers up
     public void grabbersUp(){
         leftGrabber.setPosition(LEFT_UP);
         rightGrabber.setPosition(RIGHT_UP);
